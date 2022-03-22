60343eb0150e44df15d5cb9d31f1c018570ecf49
Praveen_kumar_A1 <Praveen_kumar_A1@Dellteam.com>
2022-03-22 17:52:42 +0530

    few changes

--- src/app/service/common_service.ts
+++ src/app/service/common_service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AppConstant } from '../appconstant';
@@ -18,14 +18,7 @@ import { Airport } from '../models/airport';
       }
 
     getFlightStatus(origin: string, destination:string,date:string): Observable<any> {
-          const header = new HttpHeaders().set('Access-Control-Allow-Origin','*'); //.set('accept', 'text/plain').set('Content-Type', 'application/json')
-          
-          const httpParams = new HttpParams().set('departureDate', date)
-          .set('origin', origin)
-          .set('destination', destination);
-          const options = { params: httpParams, withCredentials: true, header };
-
-          return this.httpService.post<any>(`${AppConstant.get_flightStatus_Api}`,options
+          return this.httpService.get<any>(`${AppConstant.get_flightStatus_Api}departureDate=${date}&origin=${origin}&destination=${destination}`
           );    
       }
 
